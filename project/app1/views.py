b39e5fe277683a66c979c628fcec218f534d58ef
vaishali <guptavaishali628@gmail.com>
2024-06-24 20:45:26 +0530

    first commit

---
+++ project/app1/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse
+
+# Create your views here.
+def collage(request):
+    return HttpResponse("<center><h1>Lakshi Narayan Collage Of Technology(LNCT)</h1></center>")
